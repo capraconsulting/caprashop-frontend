31207e045e476ee86faf88d6539e82045f556ea1
Peter Hellstrand <phe@capraconsulting.no>
2014-08-22 21:45:48 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        config: {
+            app: 'app'
+        },
+        watch: {
+            styles: {
+                files: [
+                    '<%= config.app %>/styles/{,*/}*.css'
+                ]
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= config.app %>/{,*/}*.html',
+                    '<%= config.app %>/styles/{,*/}*.css'
+                ]
+            }
+        },
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                hostname: 'localhost'
+            },
+            proxies: [{
+                context: '/rest', // the context of the data service
+                host: 'localhost', // wherever the data service is running
+                port: 8080 // the port that the data service is running on
+            }],
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '.tmp',
+                        '<%= config.app %>'
+                    ]
+                },
+                middleware: function (connect, options) {
+                    var middlewares = [];
+
+                    if (!Array.isArray(options.base)) {
+                        options.base = [options.base];
+                    }
+
+                    // Setup the proxy
+                    middlewares.push(require('grunt-connect-proxy/lib/utils').proxyRequest);
+
+                    // Serve static files
+                    options.base.forEach(function(base) {
+                        middlewares.push(connect.static(base));
+                    });
+
+                    return middlewares;
+                }
+            }
+        }
+
+    });
+
+    //grunt.loadNpmTasks('grunt-connect-proxy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('serve', function (target) {
+        grunt.task.run([
+            'connect:livereload',
+            'watch'
+        ]);
+    });
+
+};
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 super-webshop-frontend
 ======================
-
 Bootcamp - repo for frontend
+
+1. Install dependencies
+$npm install
+
+2. Start the server
+$grunt serve 
+
+
+
